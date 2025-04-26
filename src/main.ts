fb7a47d20e76beb5fec1e0c0f510b57b9914a8de
Eduardo Rodriguez <eduardo110298@gmail.com>
2025-04-25 23:23:43 -0400

    chore: Added .env.example and configured prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Datalink API')
+    .setDescription(
+      'Datalink API to manage contracts, invoices, payments, plans and other data related to the ISP business.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
